bd2edf05b3fd2c23ece297cab499695a58abded3
Warrior of Jesus <kucovaleksej777@gmail.com>
2022-11-17 16:55:16 +0600

    qwerwerwersdfsdcfvxcv

--- src/itacademy/Example.java
+++ src/itacademy/Example.java
@@ -4,8 +4,5 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("hi, bro");
         int i = 1;
-        do {
-            System.out.println();
-        } while (i != 0);
     }
 }
