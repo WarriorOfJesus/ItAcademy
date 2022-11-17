dabc302d9209278554ea307b5420a2644ac41b22
WarriorOfJeses <kucovaleksej@gmail.com>
2022-11-17 16:46:46 +0600

    qwerty

--- src/itacademy/Example.java
+++ src/itacademy/Example.java
@@ -3,5 +3,9 @@ package itacademy;
 public class Example {
     public static void main(String[] args) {
         System.out.println("hi, bro");
+        int i = 1;
+        do {
+            System.out.println();
+        } while (i != 0);
     }
 }
