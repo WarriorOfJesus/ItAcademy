087e3a6daaaa63e4ea3d94fae1dc18b876e9e7c9
Warrior of Jesus <kucovaleksej777@gmail.com>
2022-11-17 18:14:47 +0600

    don't have while

--- src/itacademy/Example.java
+++ src/itacademy/Example.java
@@ -4,8 +4,5 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("hi, bro");
         int i = 1;
-        do {
-            System.out.println("hp");
-        }while(i != 0);
     }
 }
