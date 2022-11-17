dc6a1db34ffcfaae39cb7df6f7256be0ed9857b6
Warrior of Jesus <kucovaleksej777@gmail.com>
2022-11-17 18:06:21 +0600

    pushed while

--- src/itacademy/Example.java
+++ src/itacademy/Example.java
@@ -4,5 +4,8 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("hi, bro");
         int i = 1;
+        do {
+            System.out.println("hp");
+        }while(i != 0);
     }
 }
